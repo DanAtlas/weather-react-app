33ca4bea93f12bf4db17c258df2100318d848bf1
Dias Abishev <abishev295@gmail.com>
2018-11-24 00:03:37 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
 
     if (city) {
       const api_url = await 
-      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
       const data = await api_url.json();
 
       this.setState({
