b72fdc019ed1ff317982d131f940cde424fa9f1e
DanAtlas <abishev295@gmail.com>
2018-11-23 23:37:19 +0600

    Updates

---
+++ precache-manifest.9dd183d9b66d813e9c312b60e0264229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "656afcf38a27783bfdb8ddf523bd3c98",
+    "url": "/weather-react-app/static/media/mountain.656afcf3.jpg"
+  },
+  {
+    "revision": "d01157d07cdf0ece631f",
+    "url": "/weather-react-app/static/js/runtime~main.d01157d0.js"
+  },
+  {
+    "revision": "e23c7e8db3fdb0d857ba",
+    "url": "/weather-react-app/static/js/main.e23c7e8d.chunk.js"
+  },
+  {
+    "revision": "4e1b29205e28e19b4da9",
+    "url": "/weather-react-app/static/js/1.4e1b2920.chunk.js"
+  },
+  {
+    "revision": "e23c7e8db3fdb0d857ba",
+    "url": "/weather-react-app/static/css/main.46df2617.chunk.css"
+  },
+  {
+    "revision": "d13d0a6d5ce2c4676d9792d6281b0a0f",
+    "url": "/weather-react-app/index.html"
+  }
+];
